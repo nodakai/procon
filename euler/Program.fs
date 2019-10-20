805a2436de2ed506229248d6a6e1e34b571385a9
NODA, Kai <nodakai@gmail.com>
2019-10-21 00:49:46 +0800

    Project Euler 0002 in F#

--- euler/Program.fs
+++ euler/Program.fs
@@ -4,14 +4,26 @@ let euler0001() =
     let f x = x % 3 = 0 || x % 5 = 0
     seq { 1 .. 1000 - 1 } |> Seq.filter f |> Seq.sum
 
+let euler0002() =
+    let fib (a, b) =
+        if a < 4_000_000 then
+            Some(a, (b, a + b))
+        else
+            None
+    Seq.unfold fib (1, 2) |> Seq.filter (fun x -> x % 2 = 0)
+    |> Seq.sum
+
+let funcs = dict [
+    "1", euler0001;
+    "2", euler0002;
+]
+
 [<Xunit.Fact>]
 let testMain() =
-    test <@ euler0001() = 233168 @>
+    test <@ funcs.["1"]() = 233168 @>
+    test <@ funcs.["2"]() = 4613732 @>
 
 [<EntryPoint>]
 let main argv =
-    match Array.head argv with
-    | "1" -> euler0001()
-    | bad -> failwith <| sprintf "bad selection %s" bad
-    |> printfn "%d"
+    funcs.[Array.head argv]() |> printfn "%d"
     0
