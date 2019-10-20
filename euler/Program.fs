0686a2bbecab4e06d4a122d97bb89829829918de
NODA, Kai <nodakai@gmail.com>
2019-10-21 00:23:20 +0800

    Project Euler 0001 in F#

---
+++ euler/Program.fs
@@ -0,0 +1,17 @@
+﻿open Swensen.Unquote
+
+let euler0001() =
+    let f x = x % 3 = 0 || x % 5 = 0
+    seq { 1 .. 1000 - 1 } |> Seq.filter f |> Seq.sum
+
+[<Xunit.Fact>]
+let testMain() =
+    test <@ euler0001() = 233168 @>
+
+[<EntryPoint>]
+let main argv =
+    match Array.head argv with
+    | "1" -> euler0001()
+    | bad -> failwith <| sprintf "bad selection %s" bad
+    |> printfn "%d"
+    0
